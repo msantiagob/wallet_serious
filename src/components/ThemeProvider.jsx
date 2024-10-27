1bfb22488d0b5e9b447d292ac249bd629398b92b
Jay Cortes <cortesjay1991@gmail.com>
2024-10-27 17:57:20 +0100

    Header Style is done for now.

--- src/components/ThemeProvider.jsx
+++ src/components/ThemeProvider.jsx
@@ -3,22 +3,27 @@ import Icon from '../assets/icon.svg';
 
 const ThemeProvider = ({ children }) => {
   return (
-    <div className='border-b-2 border-gray-200 h-22'>
-      <div className='flex justify-between'>
-        <div className='flex'>
-          <img src={Icon} alt='' />
-          <Link>Dashboard</Link>
-          <Link>Accounts</Link>
-          <Link>Records</Link>
-          <Link>Analytics</Link>
-          <Link>Investments</Link>
-          <Link>Imports</Link>
+    <div className='border-b border-gray-200 h-20'>
+      <div className='h-20 flex justify-between items-center'>
+        <div className='flex ml-8'>
+          <img src={Icon} alt='' className='w-10 mr-5' />
+          <div className='flex gap-8 mt-2 text-gray-500'>
+            <Link>Dashboard</Link>
+            <Link>Accounts</Link>
+            <Link>Records</Link>
+            <Link>Analytics</Link>
+            <Link>Investments</Link>
+            <Link>Imports</Link>
+          </div>
         </div>
 
-        <div className='flex'>
-          <button>+ Record</button>
-          <div className=' w-8 h-8 rounded-full bg-green-800'></div>
-          <span>Elpepe</span>
+        <div className='flex items-center gap-4 mr-10'>
+          <button className='bg-blue-600 rounded-full p-1 text-white text-xs pl-3 pr-3 mr-1'>+ Record</button>
+          <div className='flex gap-2'>
+            <div className=' w-8 h-8 rounded-full bg-green-800'></div>
+            <span className='mt-1'>User</span>
+          </div>
+
         </div>
       </div>
       {children}
